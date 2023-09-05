e2014368010eca5c9b2695dc89d18de0dc2ff2c4
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-05 11:24:46 +0100

    Create clean+create.sh

---
+++ clean+create.sh
@@ -0,0 +1,21 @@
+podman stop container_package_1
+podman rm container_package_1
+podman rmi image_package_1
+
+cd /opt/dmtools/code/dmtoolv1/fastapi_orm
+
+uid=1001
+gid=1002
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+podman build -f Dockerfile -t image_package_1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_package_1 \
+--user $uid:$gid \
+localhost/image_package_1:latest
